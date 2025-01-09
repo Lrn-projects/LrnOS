8c0c184fb80be34a35259ec66ba95023caf4a1d4
Elouan Da Costa Peixoto <elouandacostapeixoto@gmail.com>
2025-01-09 20:38:38 +0100

    feat: restructure kernel project and add VGA buffer support

--- peixotos_kernel/src/main.rs
+++ peixotos_kernel/src/main.rs
@@ -1,27 +1,44 @@
 #![no_std] // don't link the Rust standard library
 #![no_main] // disable all Rust-level entry points
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
 
+mod vga_buffer;
+
 /// This function is called on panic.
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
 
-static HELLO: &[u8] = b"Hello World!";
+#[cfg(test)]
+pub fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+}
 
+#[test_case]
+fn trivial_assertion() {
+    print!("trivial assertion... ");
+    assert_eq!(1, 1);
+    println!("[ok]");
+}
+
+static HELLO: &str = "Hello from the PeixotOS kernel!";
+
+// prevent a function or static variable from being mangled when compiled
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
-
+    use core::fmt::Write;
+    vga_buffer::WRITER.lock().write_str(HELLO).unwrap();
+    println!();
+    #[cfg(test)]
+    test_main();
     loop {}
 }
-
